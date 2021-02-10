93ae054f63350df38c97640e540cca56f2954a92
Shin <2082119+shinsenter@users.noreply.github.com>
2021-02-10 14:01:20 +0900

    For v2.0

---
+++ src/fallback.js
@@ -0,0 +1,88 @@
+/**
+ *
+ * Package @shinsenter/defer.js
+ * https://www.npmjs.com/package/@shinsenter/defer.js
+ *
+ * Released under the MIT license
+ * https://raw.githubusercontent.com/shinsenter/defer.js/master/LICENSE
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019 Mai Nhut Tan <shin@shin.company>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+/* Fallback for older version of @shinsenter/defer.js */
+(function (window, defer) {
+
+    /**
+     * @deprecated Deprecated since version 2.0
+     * @function
+     * @name defer
+     * @since 1.0
+     * @param {function} func
+     * @param {number}   [delay]
+     * @see {@link Defer}
+     */
+    window.defer = defer = window.Defer;
+
+    /**
+     * @deprecated Deprecated since version 2.0
+     * @function
+     * @name deferscript
+     * @since 1.0
+     * @param {string}   src
+     * @param {string}   [id]
+     * @param {number}   [delay]
+     * @param {callback} [callback]
+     * @see {@link Defer.js}
+     */
+    window.deferscript = defer.js;
+
+    /**
+     * @deprecated Deprecated since version 2.0
+     * @function
+     * @name deferstyle
+     * @since 1.0
+     * @param {string}   src
+     * @param {string}   [id]
+     * @param {number}   [delay]
+     * @param {callback} [callback]
+     * @see {@link Defer.css}
+     */
+    window.deferstyle = defer.css;
+
+    /**
+     * @deprecated Deprecated since version 2.0
+     * @function
+     * @name deferimg
+     * @alias  deferiframe
+     * @since 1.0
+     * @param {string}   [selector]
+     * @param {number}   [delay]
+     * @param {string}   [cssclass]
+     * @param {callback} [validate]
+     * @param {object}   [observeOptions]
+     * @see {@link Defer.dom}
+     */
+    window.deferimg = window.deferiframe = defer.dom;
+
+})(this);
